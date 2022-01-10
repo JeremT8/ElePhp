4e6e9284a5daeaea03dcccee5088e81f69fb109f
JeremT8 <jeremytilletpro@gmail.com>
2022-01-10 10:58:47 +0100

    base of PhP

--- intro.php
+++ intro.php
@@ -1,3 +1,23 @@
+<?php
+
+$name = $_GET['name'];
+if(! isset($_GET["name"])){
+    $name = 'Jérémy';
+} else {
+    $name = $_GET["name"];
+}
+
+$color = $_GET['color'];
+if(! isset($_GET["color"])){
+    $color = 'green';
+} else {
+    $color = $_GET["color"];
+}
+
+$backgroundColor = 'yellowgreen';
+$age = 24;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,11 +25,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Premiere page en PhP</title>
+    <style>
+        body {
+            background-color: <?php echo $backgroundColor; ?>
+        }
+        h1 {
+            color: <?php echo $color; ?>
+        }
+
+    </style>
 </head>
 <body>
 <h1>
-    Bonjour la date du jour est :
+    Bonjour <?= $name; ?> la date du jour est :
     <?php echo date("d/m/Y"); ?>
 </h1>
+
+<pre>
+    <?php // var_dump($variableATester); permet de debugger son code // ?>
+    <?php // print_r($_SERVER) rapporte les informations du Server ?>
+</pre>
 </body>
 </html>
