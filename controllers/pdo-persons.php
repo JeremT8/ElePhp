6d4a27efad32720219b80c52a1840dbe7b22acdc
JeremT8 <jeremytilletpro@gmail.com>
2022-01-13 13:36:23 +0100

    .

--- controllers/pdo-persons.php
+++ controllers/pdo-persons.php
@@ -6,6 +6,35 @@ $db = new PDO (
 	""
 );
 
+// Recuperation des parametres du script transmi dans l'url
+$id =(int) filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+$action = filter_input(INPUT_GET, "action", FILTER_DEFAULT);
+
+
+// Verification de la presence d'une variables
+$isPosted = filter_has_var(INPUT_POST, "first_name");
+
+if($isPosted) {
+	// Recuperation des données
+	$firstName = filter_input(INPUT_POST, "first_name", FILTER_SANITIZE_STRING);
+	$lastName = filter_input(INPUT_POST, "last_name", FILTER_SANITIZE_STRING);
+
+	if(!empty(trim($lastName)) && ! empty(trim($firstName))){
+		$sql = "INSERT INTO persons (first_name, last_name) VALUES (?, ?)";
+		$statement = $db->prepare($sql);
+		$statement->execute([$firstName, $lastName]);
+		header("Location". getLinkToRoute("pdo-persons"));
+	}
+}
+
+
+// Gestion de la supression
+if($id && $action === "delete") {
+	$sql = "DELETE FROM persons WHERE id = $id";
+	$db->exec($sql);
+	header("Location:". getLinkToRoute("pdo-persons"));
+}
+
 // Requête pour lister toutes les personnes
 $sql = "SELECT * FROM persons";
 $result = $db->query($sql);
--- views/persons.php
+++ views/persons.php
@@ -1,5 +1,21 @@
-
 <h1>Liste des personnes</h1>
+
+<div class="mb-4 mt-3" >
+	<h3><strong>Ajout d'une personne</strong></h3>
+	<form method="POST" class="row p-4">
+		<div class="mb-3 col">
+			<label class="form-label"> Prénom </label>
+			<input type="text" class="form-control" name="first_name">
+		</div>
+		<div class="mb-3 col">
+			<label class="form-label"> Nom </label>
+			<input type="text" class="form-control" name="last_name">
+		</div>
+
+		<button class="btn btn-sm btn-success" name="submit">Ajout</button>
+	</form>
+</div>
+
 <table class="table table-striped table-light table-bordered">
 	<thead>
 		<tr>
@@ -9,11 +25,15 @@
 		</tr>
 	</thead>
 	<tbody>
-		<?php foreach ($personList as $person): ?>
+		<?php foreach ($personList as $person) : ?>
 			<tr>
 				<td><?= $person->first_name ?></td>
 				<td><?= $person->last_name ?></td>
-				<td></td>
+				<td>
+					<a href="<?= getLinkToRoute("pdo-persons", ["id" => $person->id, "action" => "delete"]) ?>" class="btn btn-sm btn-danger" onclick="return confirm('Etes vous sur de vouloir supprimer cette personne ?')">Supprimer
+					</a>
+					<!-- <a href="<?= getLinkToRoute("pdo-persons", ["id" => $person->id, "action" => "delete"]) ?>" class="btn btn-sm btn-danger">Supprimer</a> -->
+				</td>
 			</tr>
 		<?php endforeach; ?>
 	</tbody>
