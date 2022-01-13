1dbf38b4d72b6d7a9ff6cd82ee13df3b7b4456cc
JeremT8 <jeremytilletpro@gmail.com>
2022-01-13 14:00:23 +0100

    update

--- controllers/pdo-persons.php
+++ controllers/pdo-persons.php
@@ -1,38 +1,50 @@
-<?php 
+<?php
 // Connexion au serveur de la BD
-$db = new PDO (
+$db = new PDO(
 	"mysql:host=127.0.0.1;dbname=php_cda_2022;charset=utf8",
 	"root",
 	""
 );
 
 // Recuperation des parametres du script transmi dans l'url
-$id =(int) filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+$id = (int) filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 $action = filter_input(INPUT_GET, "action", FILTER_DEFAULT);
 
 
 // Verification de la presence d'une variables
 $isPosted = filter_has_var(INPUT_POST, "first_name");
 
-if($isPosted) {
+if ($isPosted) {
 	// Recuperation des données
 	$firstName = filter_input(INPUT_POST, "first_name", FILTER_SANITIZE_STRING);
 	$lastName = filter_input(INPUT_POST, "last_name", FILTER_SANITIZE_STRING);
 
-	if(!empty(trim($lastName)) && ! empty(trim($firstName))){
+	if (!empty(trim($lastName)) && !empty(trim($firstName))) {
 		$sql = "INSERT INTO persons (first_name, last_name) VALUES (?, ?)";
 		$statement = $db->prepare($sql);
 		$statement->execute([$firstName, $lastName]);
-		header("Location". getLinkToRoute("pdo-persons"));
+		header("Location" . getLinkToRoute("pdo-persons"));
 	}
 }
 
 
 // Gestion de la supression
-if($id && $action === "delete") {
+if ($id && $action === "delete") {
 	$sql = "DELETE FROM persons WHERE id = $id";
 	$db->exec($sql);
-	header("Location:". getLinkToRoute("pdo-persons"));
+	header("Location:" . getLinkToRoute("pdo-persons"));
+}
+
+// En cas de modification, récupération des infos de la personne à modifier
+if ($id && $action === "update") {
+	$sql = "SELECT * FROM persons WHERE id = $id";
+	$result = $db->query($sql);
+	$currentPerson = $result->fetch(PDO::FETCH_OBJ);
+} else {
+	$currentPerson = new StdClass();
+	$currentPerson->first_name = "";
+	$currentPerson->last_name = "";
+	$currentPerson->id = null;
 }
 
 // Requête pour lister toutes les personnes
@@ -47,9 +59,10 @@ $data = $result->fetchAll(PDO::FETCH_OBJ);
 echo render(
 	"persons",
 	[
-		"personList" => $data
+		"personList" => $data,
+		"currentPerson" => $currentPerson
 	]
-	);
+);
 
 
 
@@ -57,25 +70,22 @@ echo render(
 /**
  * Récupération des résultat dans une boucle while
  * $row = $result->fetch(PDO::FETCH_OBJ);
-*
-*  while ( $row !== false)  {
-*	echo "<p> {$row->first_name} {$row->last_name} </p>";
-*	$row = $result->fetch(PDO::FETCH_OBJ);
-*}
-*/
+ *
+ *  while ( $row !== false)  {
+ *	echo "<p> {$row->first_name} {$row->last_name} </p>";
+ *	$row = $result->fetch(PDO::FETCH_OBJ);
+ *}
+ */
 
 
 
 /**
  * Récuperation des résultats un à un
  * $row = $result->fetch(PDO::FETCH_ASSOC);
-* var_dump($row);
-* echo "<p> {$row['first_name']} {$row['last_name']} </p>"; 
-* 
-* $row = $result->fetch(PDO::FETCH_NUM);
-* var_dump($row);
-* echo "<p> {$row[2]} {$row[1]} </p>"; 
+ * var_dump($row);
+ * echo "<p> {$row['first_name']} {$row['last_name']} </p>"; 
+ * 
+ * $row = $result->fetch(PDO::FETCH_NUM);
+ * var_dump($row);
+ * echo "<p> {$row[2]} {$row[1]} </p>"; 
  */
-
-
-
--- views/persons.php
+++ views/persons.php
@@ -1,16 +1,17 @@
 <h1>Liste des personnes</h1>
 
-<div class="mb-4 mt-3" >
+<div class="mb-4 mt-3">
 	<h3><strong>Ajout d'une personne</strong></h3>
 	<form method="POST" class="row p-4">
 		<div class="mb-3 col">
 			<label class="form-label"> Prénom </label>
-			<input type="text" class="form-control" name="first_name">
+			<input type="text" class="form-control" name="first_name" value="<?= $currentPerson->first_name ?>">
 		</div>
 		<div class="mb-3 col">
 			<label class="form-label"> Nom </label>
-			<input type="text" class="form-control" name="last_name">
+			<input type="text" class="form-control" name="last_name" value="<?= $currentPerson->last_name ?>">
 		</div>
+		<input type="hidden" name="id" value="<?= $currentPerson->id ?>">
 
 		<button class="btn btn-sm btn-success" name="submit">Ajout</button>
 	</form>
@@ -22,6 +23,8 @@
 			<td>Prénom</td>
 			<td>Nom</td>
 			<td></td>
+			<td></td>
+
 		</tr>
 	</thead>
 	<tbody>
@@ -32,7 +35,10 @@
 				<td>
 					<a href="<?= getLinkToRoute("pdo-persons", ["id" => $person->id, "action" => "delete"]) ?>" class="btn btn-sm btn-danger" onclick="return confirm('Etes vous sur de vouloir supprimer cette personne ?')">Supprimer
 					</a>
-					<!-- <a href="<?= getLinkToRoute("pdo-persons", ["id" => $person->id, "action" => "delete"]) ?>" class="btn btn-sm btn-danger">Supprimer</a> -->
+				</td>
+				<td>
+					<a href="<?= getLinkToRoute("pdo-persons", ["id" => $person->id, "action" => "update"]) ?>" class="btn btn-sm btn-primary">Modifier
+					</a>
 				</td>
 			</tr>
 		<?php endforeach; ?>
