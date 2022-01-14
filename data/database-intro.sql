cf6eb1ad9fc9be52df4c1d6ca01a1f7f5fdb73de
JeremT8 <jeremytilletpro@gmail.com>
2022-01-14 13:38:46 +0100

    .

--- data/database-intro.sql
+++ data/database-intro.sql
@@ -40,3 +40,62 @@ CREATE TABLE IF NOT EXISTS orders (
     date_commande DATE NOT NULL,
     montant DECIMAL(6,2) NOT NULL
 );
+
+
+-- Permet de filtrer dans la table persons pour la ligne first_name commencant par un a.
+SELECT * FROM persons WHERE first_name LIKE 'A%';
+
+
+-- Permet de filtrer dans la table persons pour la ligne first_name qui correspond au info dans le tableau apres le IN.
+SELECT * FROM persons WHERE first_name IN ('Martin', 'Tillet');
+
+
+-- Donne le resultat de toutes les persons dont le prenom est different de 'Martin'
+SELECT * FROM persons WHERE first_name != 'Martin';
+
+
+DROP TABLE IF EXISTS `livres_simples`;
+CREATE TABLE `livres_simples` (
+`id` mediumint unsigned NOT NULL AUTO_INCREMENT,
+`titre` varchar(80) NOT NULL,
+`prix` decimal(5,2) NOT NULL,
+`auteur` varchar(50) NOT NULL,
+`editeur` varchar(50) NOT NULL,
+`genre` varchar(50) NOT NULL,
+`date_publication` date DEFAULT NULL,
+`nationalite_auteur` varchar(50) DEFAULT NULL,
+`langue` varchar(50) DEFAULT NULL,
+`auteur_prenom` varchar(50) DEFAULT NULL,
+`auteur_nom` varchar(50) DEFAULT NULL,
+PRIMARY KEY (`id`)
+);
+
+
+-- Recuperer tous les livres de la catégorie Informatique ecris en Francais
+SELECT * FROM livres_simples WHERE genre = 'Informatique' AND langue = 'français';
+
+
+-- Recuperer tous les livres ecrits en italien et en castillan
+SELECT * FROM livres_simples WHERE langue = 'italien' OR langue = 'castillan'; 
+-- OU
+SELECT * FROM livres_simples WHERE langue IN ('italien','castillan'); 
+
+
+-- Recuperer les livres qui NE sont PAS ecrits en Anglais et dont le prix est inferieur à 12
+SELECT * FROM livres_simples WHERE langue != 'anglais' AND prix < 12 ORDER BY DEC;
+
+-- Les livres edité par Hachette
+SELECT * FROM livres_simples WHERE editeur = 'Hachette';
+
+
+-- Les livres ecrits par Joe Celko 
+SELECT * FROM livres_simples WHERE auteur_nom = 'Celko' AND auteur_prenom = 'Joe';
+
+
+
+
+
+
+
+
+
